3f2cd6c116998b30eeab2dfda5dd3e88c6adaa3a
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-04-09 19:21:41 +0530

    book.h
    Added TextBook and StoryBook Class 
    Implementation in the main is required
    lines(50->135)
    Used Inheritance and virtual function

--- book.h
+++ book.h
@@ -9,7 +9,7 @@ class book
     char bname[50];//bookname
     char aname[20];//authorname
 public:
-    void createbook()
+    virtual void createbook()
     {
         cout<<"\nNEW BOOK ENTRY...\n";
         cout<<"\nENTER BOOK NO.";
@@ -20,7 +20,7 @@ public:
         gets(aname);
         cout<<"\n\n\nBook Created..";
     }
-    void showbook()
+    virtual void showbook()
     {
         cout<<"\nBook Number: "<<bno;
         cout<<"\nBook Name: ";
@@ -28,7 +28,7 @@ public:
         cout<<"\nBook Author Name: ";
         puts(aname);
     }
-    void modifybook()
+    virtual void modifybook()
     {
         cout<<"\nBook Number: "<<bno;
         cout<<"\nModify Book Name :";
@@ -48,3 +48,89 @@ public:
         cout<<bno<<setw(30)<<bname<<setw(30)<<aname<<endl;
     }
 };//class ends here
+
+class Textbook : protected book
+{
+private:
+    string Academic_Level;
+    int NoInStock;
+    int NumberOfIssues;
+public:
+    void createbook() override
+    {
+        book::createbook();
+        cout<<"\nAs it is a text following fields are also required"<<endl;
+        cout<<"Enter Academic level i.e. UG , PG , PHD ..etc"<<endl;
+        getline(cin,Academic_Level);
+        cout<<"Enter Amount in stock"<<endl;
+        cin>>NoInStock;
+        cout<<"Enter No of issues"<<endl;
+        cin>>NumberOfIssues;
+    }
+    void showbook() override
+    {
+        book::showbook();
+        cout<<"Reccomended for "<<Academic_Level<<"Students"<<endl;
+        cout<<"No of books in stock is "<<NoInStock<<endl;
+        cout<<"Has "<<NumberOfIssues<<"Issues"<<endl;
+    }
+    void modifybook() override
+    {
+        book::modifybook();
+        cout<<"\nModify Academic level i.e. UG , PG , PHD ..etc"<<endl;
+        getline(cin,Academic_Level);
+        cout<<"Modify Amount in stock"<<endl;
+        cin>>NoInStock;
+        cout<<"Modify No of issues"<<endl;
+        cin>>NumberOfIssues;
+    }
+};
+class StoryBook : protected book
+{
+private:
+    string Genre;
+    string Type;//Novel , shortStory , Biography..etc
+    bool IsIssueAllowed;
+public:
+    void createbook() override
+    {
+        book::createbook();
+        cout<<"Enter Genre  i.e. Drama , Adventure , SelfHelp..etc"<<endl;
+        getline(cin,Genre);
+        cout<<"Enter Type  i.e. Novel , shortStory , Biography..etc"<<endl;
+        getline(cin,Type);
+        char choice;
+        cout<<"Is Issue allowed y/n";
+        cin>>choice;
+        if(choice == 'y')
+            IsIssueAllowed =true;
+        else
+            IsIssueAllowed = false;
+    }
+    void showbook() override
+    {
+        book::showbook();
+        cout<<"Genre is  "<<Genre<<endl;
+        cout<<"and is of type "<<Type<<endl;
+        if (IsIssueAllowed)
+            cout<<"Allowed to issue"<<endl;
+        else
+            cout<<"Not allowed to issue"<<endl;
+    }
+    void modifybook() override
+    {
+        book::modifybook();
+        cout<<"Modify Genre  i.e. Drama , Adventure , SelfHelp..etc"<<endl;
+        getline(cin,Genre);
+        cout<<"Modify Type  i.e. Novel , shortStory , Biography..etc"<<endl;
+        getline(cin,Type);
+        char choice;
+        cout<<"Is Issue allowed y/n";
+        cin>>choice;
+        if(choice == 'y')
+            IsIssueAllowed =true;
+        else
+            IsIssueAllowed = false;
+    }
+};
+
